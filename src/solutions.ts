1cd8d4040580437e3e3eae549937efa32f9b362d
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-05-07 18:39:02 +0600

    complete problem-3

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,11 +1,22 @@
-
+// Problem 1:
 const filterEvenNumbers = (array: number[]): number[] => {
     const result = array.filter((num) => num % 2 === 0);
     return result;
 }
 
-
+// Problem 2:
 const reverseString = (str: string) => {
-    const result = str.split("").reverse().join("")
+    const result = str.split("").reverse().join("");
     return result;
 }
+
+// Problem 3:
+type StringOrNumber = string | number
+const checkType = (input: StringOrNumber): StringOrNumber => {
+    if (typeof input === "string") {
+        return "string"
+    }
+    else return "number"
+
+}
+
