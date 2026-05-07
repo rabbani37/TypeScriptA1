ef3d4a628f37c10fe15b470767ffc67ed307fc55
georabbani <231213066+rabbani37@users.noreply.github.com>
2026-05-07 18:42:06 +0600

    complete problem-6

--- src/solutions.ts
+++ src/solutions.ts
@@ -36,3 +36,24 @@ const toggleReadStatus = (bookObj: Book) => {
         isRead: true
     }
 }
+
+
+// Problem: 6
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+}
+class Student extends Person {
+    grade: string;
+    constructor(name: string, age: number, grade: string) {
+        super(name, age)
+        this.grade = grade
+    }
+    getDetails() {
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+}
